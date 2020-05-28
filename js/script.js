e34170296baf7cec97fa83acfa4167b1750f92eb
Emil <emilkr11@gmail.com>
2020-05-28 16:06:40 +0200

    Landingpage done in mobile version

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+
+    function setSizes() {
+        let containerHeight = $("#gradient").height();
+        $("#gradient").height(containerHeight - 120);
+    }
+
+    $(document).ready(function() { setSizes(); });
+
+
+
+});
